61b768e994167ed083f218335164088e2924e5bc
Trayana Boykova <trayana.boykova@gmail.com>
2025-11-06 11:06:12 +0200

    completed stage 1

---
+++ Readability Score (Java)/task/src/readability/Main.java
@@ -0,0 +1,16 @@
+package readability;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        if (text.length() > 100) {
+            System.out.println("HARD");
+        } else {
+            System.out.println("EASY");
+        }
+    }
+}
